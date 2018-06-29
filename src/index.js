f975f1ba434e5e61640be80ed8265fc926a33759
Luke Karrys <lukekarrys@gmail.com>
2018-06-29 15:22:01 -0700

    pass undefined for crdt name when no hash

--- src/index.js
+++ src/index.js
@@ -4,9 +4,5 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const props = {}
-const name = window.location.hash.substr(1)
-if (name) props.crdtName = name
-
-ReactDOM.render(<App {...props} />, document.getElementById('root'));
+ReactDOM.render(<App crdtName={window.location.hash.substr(1) || undefined} />, document.getElementById('root'));
 registerServiceWorker();

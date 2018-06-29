59d7016cf37049026095087b1e43551e5034b0a5
Luke Karrys <lukekarrys@gmail.com>
2018-06-29 14:31:10 -0700

    reactify

--- src/App.js
+++ src/App.js
@@ -1,18 +1,75 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const XIPFS = require('ipfs')
+const PeerCRDT = require('peer-crdt')
+const PeerCRDTIPFS = require('peer-crdt-ipfs')
+const encrypt = require('peer-crdt/test/helpers/encrypt')
+const decrypt = require('peer-crdt/test/helpers/decrypt')
+const Showdown = require('showdown')
+const TextareaBinding = require('peer-crdt-pad')
+
 class App extends Component {
+  static defaultProps = {
+    crdtName: 'something5'
+  }
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      output: ''
+    }
+
+    const ipfs = new XIPFS({
+      EXPERIMENTAL: {
+        pubsub: true
+      }, config:  {
+        Addresses: {
+          Swarm: [
+            '/dns4/protocol.andyet.net/tcp/9090/ws/p2p-websocket-star'
+          ]
+        },
+        Bootstrap: []
+      }
+    })
+
+    const crdtipfs = PeerCRDTIPFS(ipfs)
+
+    const crdt = PeerCRDT.defaults({
+        ...crdtipfs,
+        signAndEncrypt: encrypt,
+        decryptAndVerify: decrypt
+    })
+    window.crdt = crdt
+
+    this._data = crdt.create('treedoc-text', props.crdtName)
+    window.data = this._data
+
+    this._converter = new Showdown.Converter()
+  }
+
+  componentDidMount() {
+    this._data.network.start().then(() => {
+      window.binding = new TextareaBinding(this._data, this._textarea)
+      this.setMarkdown()
+      setTimeout(this.setMarkdown, 500)
+    })
+  }
+
+  setMarkdown = () => {
+    this.setState({
+      output: this._converter.makeHtml(this._textarea.value)
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="input">
+          <textarea ref={(c) => this._textarea = c} className="input-area" />
+        </div>
+        <div className="output" dangerouslySetInnerHTML={{ __html: this.state.output }} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -4,5 +4,9 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const props = {}
+const name = window.location.hash.substr(1)
+if (name) props.crdtName = name
+
+ReactDOM.render(<App {...props} />, document.getElementById('root'));
 registerServiceWorker();
